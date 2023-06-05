696b661218d7de2dafa2b327276e0ac48275e9f6
Su Hlaing Thin <115348141+suhiaing@users.noreply.github.com>
2023-06-05 19:58:13 +0630

    Update The_Binomial_Theorem.py

--- The_Binomial_Theorem.py
+++ The_Binomial_Theorem.py
@@ -1,3 +1,19 @@
-my_ls = [["Hello","Hi"],["kids"],["World"]]
+# my_ls = [2,3]
+power = 4
+def get_coefficient(power):
+    coefficient = [1]
+    divisor = 1
+    multiplication = 1
+    for i in reversed(range(1, power+1)):
+        multiplication = multiplication * i
+        coefficient.append(multiplication)
+    print("Before division, coefficient be like:",coefficient)
+    
+    
+    for i in range(1,power+1):
+        divisor = divisor * i
+        print("dividing",coefficient[i],"with",divisor)
+        coefficient[i] = coefficient[i] / divisor
+    print("After all coefficient are:",coefficient)
 
-print(my_ls[0][0]) 
+get_coefficient(power)
